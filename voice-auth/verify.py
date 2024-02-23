8817a044bc52a78570687e271677e0870abec7d0
CUI Hao <cuihao.leo@gmail.com>
2024-02-22 17:02:23 -0800

    Voice authentication demo code

---
+++ voice-auth/verify.py
@@ -0,0 +1,8 @@
+from speechbrain.pretrained import SpeakerRecognition
+import sys
+
+f1, f2 = sys.argv[1:]
+
+verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
+score, prediction = verification.verify_files(f1, f2)
+print(score.item(), prediction.item())
